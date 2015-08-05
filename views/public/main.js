df750a2bbaf5b455927f04863d44b3c8875db345
Sebastian Hardt <sebasth@gmx.de>
2015-08-05 22:27:51 +0200

    Got it kinda running

--- views/public/main.js
+++ views/public/main.js
@@ -1,12 +1,66 @@
 
 $(function() {
   var socket = io('http://localhost:8080');
-  socket.on('topicData', function (data) {
+  /*socket.on('topicData', function (data) {
     console.log(data);
-  });
+  });*/
 
 
   $('#container').highcharts({
+    title: { text: 'Real Time Samples' },
+    xAxis: {
+			type: 'datetime',
+			tickPixelInterval: 100
+		},
+		yAxis: {
+			title: { text: 'Temp' },
+			tickInterval: 10,
+			min: 0,
+			max: 100
+		},
+		tooltip: {
+            formatter: function() {
+                return '<b>'+ this.series.name + '</b><br/>'
+                    + '[ ' + Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x)
+                    + ' , '
+                    + this.y + ' ]';
+            }
+		},chart: {
+			type: 'spline',
+			renderTo: 'graph_container',
+			events: {
+				load: function() {
+					var series = this.series[0];
+
+				    socket.on('topicData', function (data) {
+				    	// when a sample arrives we plot it
+              console.log(data);
+						series.addPoint([data.time, parseInt(data.payload)], true, true);
+				    });
+				}
+			}
+		},
+        series: [{
+            name: 'Temp',
+            data: (function() {
+                // generate some points to render before real samples arrive from feed
+                var data = [];
+
+                var time = (new Date()).getTime(),
+                   i;
+               // 20 samples, starting 19 ms ago up to present time when feed starts plotting
+               for (i = -19; i <= 0; i++) {
+                   data.push({
+                       x: time + (i * 1000),
+                       y: 0
+                   });
+               }
+
+                return data;
+            })()
+        }]
+  });
+  /*$('#container').highcharts({
       xAxis: {
           categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
               'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
@@ -14,5 +68,5 @@ $(function() {
       series: [{
           data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
       }]
-  });
+  });*/
 });
