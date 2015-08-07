ae5cd37fb034bf21be70ec350411c833092f8924
Sebastian Hardt <s.hardt@micromata.de>
2015-08-07 10:07:39 +0200

    Topics and panels are splitted now

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // well we need som logging
 var log4js = require('log4js');
 var logger = log4js.getLogger();
@@ -10,54 +9,64 @@ var cfg = require('./config.json');
 logger.debug(cfg);
 
 // init the mqtt client
+var subscribtedTopics = [];
 var mqtt_logger = log4js.getLogger('[mqtt]');
-var mqtt    = require('mqtt');
+var mqtt = require('mqtt');
 var panels_cfg = require('./panels.json');
-var client  = mqtt.connect('mqtt://'+cfg.mqttClient.host);
-client.on('connect', function () {
-  mqtt_logger.debug("Mqtt connected: "+cfg.mqttClient.host);
-  for (var panel in panels_cfg.panels) {
-    var topic = panels_cfg.panels[panel].topic;
-    mqtt_logger.debug("Subscribing to: "+topic)
+var client = mqtt.connect('mqtt://' + cfg.mqttClient.host);
+client.on('connect', function() {
+  mqtt_logger.debug("Mqtt connected: " + cfg.mqttClient.host);
+  for (var topicIdx in panels_cfg.topics) {
+    var topic = panels_cfg.topics[topicIdx];
+    if (subscribtedTopics[topic] != null) {
+      continue;
+    }
+    subscribtedTopics[topic] = true;
+    mqtt_logger.debug("Subscribing to: " + topic)
     client.subscribe(topic);
   }
 });
 
-client.on('message', function (topic, message) {
+client.on('message', function(topic, message) {
   // message is Buffer
-  mqtt_logger.debug("Got message on: "+topic)
-  console.log(message.toString());
-  io.emit('topicData_'+topic, { "topic": topic, "time" : new Date().getTime() , "payload" : message.toString()});
+  mqtt_logger.debug("Got message on: " + topic)
+  io.emit('topicData_' + topic, {
+    "topic": topic,
+    "time": new Date().getTime(),
+    "payload": message.toString()
+  });
 });
 
 // the express stuff
 var exp_logger = log4js.getLogger('[express]');
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var app = express();
 
 // io
 var io_logger = log4js.getLogger('[socket io]');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-io.on('connection', function (socket) {
-  io_logger.debug("Got connection from: "+socket.id);
-  socket.emit('panelsCfg',panels_cfg);
+io.on('connection', function(socket) {
+  io_logger.debug("Got connection from: " + socket.id);
+  socket.emit('panelsCfg', panels_cfg);
 });
 
 // handlebars stuff
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
 // bower
-app.use('/bower',  express.static(__dirname + '/views/bower'));
-app.use('/public',  express.static(__dirname + '/views/public'));
+app.use('/bower', express.static(__dirname + '/views/bower'));
+app.use('/public', express.static(__dirname + '/views/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.render('home');
 });
 
-server.listen(cfg.express.port, function () {
+server.listen(cfg.express.port, function() {
   var host = server.address().address;
   var port = server.address().port;
 
